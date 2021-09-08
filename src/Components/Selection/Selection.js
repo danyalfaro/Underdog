1d17e1899b0c237d540125b445eb73eff9c5fda1
Danyalfaro <daniel.alfaro@upr.edu>
2021-09-07 22:30:47 -0400

    Artist images now appear rounded.

--- src/Components/Selection/Selection.js
+++ src/Components/Selection/Selection.js
@@ -1,34 +1,43 @@
-import React from 'react';
-import './Selection.scss';
+import React from "react";
+import "./Selection.scss";
 
 class Selection extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    removeSeed = () => {
-        this.props.removeSeed(this.props.id);
-    }
+  removeSeed = () => {
+    this.props.removeSeed(this.props.id);
+  };
 
-    setRemove = () => {
-        if(this.props.type === 'seed'){
-            return (<button className="removeBtn" onClick={this.removeSeed}>X</button>);
-        }else{
-            return;
-        }
+  setRemove = () => {
+    if (this.props.type === "seed") {
+      return (
+        <button className="removeBtn" onClick={this.removeSeed}>
+          X
+        </button>
+      );
+    } else {
+      return;
     }
+  };
 
-    render() { 
-        return ( 
-        <div className="selectionWrapper">
-            <img src={this.props.imageUrl} alt="cover" width="190px"></img>
-            {this.setRemove()}
-            <div className="selectionName">{this.props.name}</div>
-            <div className="selectionArtist">{this.props.artist}</div>
-        </div> 
-        );
-    }
+  render() {
+    return (
+      <div className="selectionWrapper">
+        <img
+          className={this.props.artist ? "songImage" : "artistImage"}
+          src={this.props.imageUrl}
+          alt="cover"
+          width="190px"
+        ></img>
+        {this.setRemove()}
+        <div className="selectionName">{this.props.name}</div>
+        <div className="selectionArtist">{this.props.artist}</div>
+      </div>
+    );
+  }
 }
- 
+
 export default Selection;
