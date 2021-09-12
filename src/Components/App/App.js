9d0b182e9a3593fb33a13a7b895a31325029b4b5
Danyalfaro <daniel.alfaro@upr.edu>
2021-09-12 14:10:26 -0400

    Redirects to spotify login from start.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,9 @@
-import './App.scss';
-import Spotify from '../../util/Spotify';
-import React from 'react';
-import SearchBar from '../SearchBar/SearchBar';
-import ResultsList from '../ResultsList/ResultsList';
-import SelectionList from '../SelectionList/SelectionList';
+import "./App.scss";
+import Spotify from "../../util/Spotify";
+import React from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import ResultsList from "../ResultsList/ResultsList";
+import SelectionList from "../SelectionList/SelectionList";
 
 class App extends React.Component {
   constructor(props) {
@@ -17,74 +17,84 @@ class App extends React.Component {
   }
 
   search = (term, searchType) => {
-    if(searchType === "artist"){
-      Spotify.searchArtist(term).then(searchResults => {
-        this.setState({searchResults: searchResults});
+    if (searchType === "artist") {
+      Spotify.searchArtist(term).then((searchResults) => {
+        this.setState({ searchResults: searchResults });
       });
-    }else{
-      Spotify.search(term).then(searchResults => {
-        this.setState({searchResults: searchResults});
+    } else {
+      Spotify.search(term).then((searchResults) => {
+        this.setState({ searchResults: searchResults });
       });
     }
-  }
+  };
 
   addSeed = (name, track, artist, image) => {
-    if(this.state.seed.length < 3){
-      if(this.state.seed.some(elem => elem.id === track)){
+    if (this.state.seed.length < 3) {
+      if (this.state.seed.some((elem) => elem.id === track)) {
         alert("Song already selected!");
-      }else{
+      } else {
         let newSeedList = this.state.seed;
-        if(!artist){
-          let newSeedObject = {name: name, id: track, image: image};
+        if (!artist) {
+          let newSeedObject = { name: name, id: track, image: image };
           newSeedList.push(newSeedObject);
-          this.setState({seed: newSeedList}, this.getRecommended);
-        }else{
-          let newSeedObject = {name: name, id: track, artist: artist, image: image};
+          this.setState({ seed: newSeedList }, this.getRecommended);
+        } else {
+          let newSeedObject = {
+            name: name,
+            id: track,
+            artist: artist,
+            image: image,
+          };
           newSeedList.push(newSeedObject);
-          this.setState({seed: newSeedList}, this.getRecommended);
+          this.setState({ seed: newSeedList }, this.getRecommended);
         }
       }
-        
     }
-  }
+  };
 
   removeSeed = (track) => {
-    let newSeedList = this.state.seed.filter(seed => seed.id !== track);
-    this.setState({seed: newSeedList}, this.getRecommended);
-  }
+    let newSeedList = this.state.seed.filter((seed) => seed.id !== track);
+    this.setState({ seed: newSeedList }, this.getRecommended);
+  };
 
   getRecommended = () => {
-    if(this.state.seed.length > 0){
-      Spotify.getRecommended(this.state.seed).then(recommendedResult => {
-        this.setState({recommended: recommendedResult});
-      })
+    if (this.state.seed.length > 0) {
+      Spotify.getRecommended(this.state.seed).then((recommendedResult) => {
+        this.setState({ recommended: recommendedResult });
+      });
     }
-  }
+  };
 
   createPlaylist = () => {
-    let uris = this.state.recommended.map(elem => elem.uri);
+    let uris = this.state.recommended.map((elem) => elem.uri);
     let playlistName = "";
-    for(let i=0; i<this.state.seed.length; i++){
-      if(i === 0){
+    for (let i = 0; i < this.state.seed.length; i++) {
+      if (i === 0) {
         playlistName += this.state.seed[0].name;
-      }else{
+      } else {
         playlistName += ` + ${this.state.seed[i].name}`;
       }
     }
     Spotify.savePlaylist(playlistName, uris);
+  };
+
+  componentDidMount() {
+    //As app is mounted, redirect to spotify login for first time users.
+    Spotify.getAccessToken();
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          underdog
-        </header>
+        <header className="App-header">underdog</header>
         <div className="App-body">
           <div className="searchSection">
             <SearchBar onSearch={this.search} />
             <div className="resultsList">
-              <ResultsList results={this.state.searchResults} addSeed={this.addSeed}/>
+              <ResultsList
+                results={this.state.searchResults}
+                addSeed={this.addSeed}
+              />
             </div>
           </div>
           <div className="divider"></div>
@@ -94,7 +104,12 @@ class App extends React.Component {
                 <div className="detailWording">seeds</div>
                 <div className="detailHighlight"></div>
               </div>
-              <SelectionList selection={this.state.seed} seed={this.state.seed} selectionType="seed" removeSeed={this.removeSeed}/>
+              <SelectionList
+                selection={this.state.seed}
+                seed={this.state.seed}
+                selectionType="seed"
+                removeSeed={this.removeSeed}
+              />
             </div>
             <div className="recommendedList">
               <div className="recommendedDetail">
@@ -102,10 +117,19 @@ class App extends React.Component {
                 <div className="detailHighlight"></div>
               </div>
               <div className="selectionList">
-                <SelectionList selection={this.state.recommended} seed={this.state.seed}/>
+                <SelectionList
+                  selection={this.state.recommended}
+                  seed={this.state.seed}
+                />
               </div>
             </div>
-            <button type="button" className="playlistButton" onClick={this.createPlaylist}>Save to Playlist</button>
+            <button
+              type="button"
+              className="playlistButton"
+              onClick={this.createPlaylist}
+            >
+              Save to Playlist
+            </button>
           </div>
         </div>
       </div>
